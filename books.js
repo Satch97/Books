2111a94b9a709730e8e79b384180a7767fbdec3e
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-24 18:06:51 +0000

    Read response for category list and add buttons

--- books.js
+++ books.js
@@ -1,4 +1,16 @@
-function printCategories(ajax) {}
+function printCategories(ajax) {
+  var categories = ajax.responseXML.getElementsByTagName("category");
+  for (let i = 0; i < categories.length; i++) {
+    category = categories[i].firstChild.nodeValue;
+    radio = getRadioButton(category);
+    label = getLabel(category);
+    $("controls").appendChild(radio);
+    $("controls").appendChild(label);
+  }
+
+    button = getListButton();
+    $("controls").appendChild(button);
+}
 
 
 window.onload = function () {
