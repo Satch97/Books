a0154a2d5429d51a307635ac6bd2a5e28c7a2fd0
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-24 00:58:07 +0000

    Add necessary scripts and elements

--- books.php
+++ books.php
@@ -2,9 +2,18 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <title>Books</title>
+    <meta name="Books" content="">
+    <script src="src/prototype.js"> </script>
+    <script src="books.js"> </script>
 </head>
 
 <body>
+
+    <div id = "controls" name = "planning area"></div>
+    <div id = "booklist" name =""></div>
+
 </body>
 
 </html>
