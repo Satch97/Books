ec1bc64a7a7361ed7306f1505702c4d30b3f97b3
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-24 18:09:39 +0000

    implement getradioinputs:

--- books.js
+++ books.js
@@ -1,3 +1,12 @@
+function getRadioButton(category_name) {
+  var radiobtn = document.createElement("input");
+  radiobtn.type = "radio";
+  radiobtn.name = "category";
+  radiobtn.value = category_name;
+  return radiobtn;
+}
+
+
 function printCategories(ajax) {
   var categories = ajax.responseXML.getElementsByTagName("category");
   for (let i = 0; i < categories.length; i++) {
