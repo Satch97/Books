0b4e7f1725df3da4bfba40922cad18025216af45
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-24 17:57:07 +0000

    Wrap php to only handle one request

--- booklist.php
+++ booklist.php
@@ -1,7 +1,18 @@
 <?php
+  if(isset($_GET['categorylist'])){
+
   try {
-    $db = new PDO('mysql:dbname=booksdb;host=localhost', 'reader', 'bookworm');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $db = new PDO('mysql:dbname=booksdb;host=localhost', 'reader', 'bookworm');
+     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
   } catch (PDOException $ex) {
+
+    echo ("failed request");
   }
+  $xmlresponse .= "</categories>\n";
+  echo($xmlresponse);
+
+  exit;
+  }
+
 ?>
