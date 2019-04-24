19ad90b53e2659b0904348048caf28427d8e0dc4
Satchin Campbell <satchin.campbell@gmail.com>
2019-04-24 17:58:06 +0000

    Request from database and write categories to xml response

--- booklist.php
+++ booklist.php
@@ -5,6 +5,19 @@
      $db = new PDO('mysql:dbname=booksdb;host=localhost', 'reader', 'bookworm');
      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+     $categories = $db->query("SELECT cat_name FROM category;");
+
+     $xmlresponse = "<?xml version='1.0' encoding='utf-8'?>\n";
+     $xmlresponse .= "<categories>\n";
+
+     header("Content-type: text/xml; charset=utf-8");
+
+     foreach ($categories as $category) {
+       $xmlresponse .= "<category>";
+       $xmlresponse .= $category["cat_name"];
+       $xmlresponse .= "</category>\n";
+     }
+
   } catch (PDOException $ex) {
 
     echo ("failed request");
